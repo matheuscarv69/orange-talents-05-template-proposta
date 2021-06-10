dcbfeb468b4b4d97869e7d2f71629c406f43e952
matheuscarv69 <matheus9126@gmail.com>
2021-06-10 19:25:08 -0400

    :sparkles: feat: feature for login with keycloak

---
+++ src/main/java/proposta/configs/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package proposta.configs.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.POST, "/api/proposal/**").hasAuthority("SCOPE_propostas-scope:write")
+                        .antMatchers(HttpMethod.GET, "/api/proposal/**").hasAuthority("SCOPE_propostas-scope:read")
+                        .antMatchers(HttpMethod.POST, "/api/biometry/**").hasAuthority("SCOPE_propostas-scope:write")
+                        .anyRequest().authenticated()
+        ).csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+}
